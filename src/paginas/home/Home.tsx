c1f6db1b3de3d663e8a8a80243f3f5bf6afd2471
Adriano <adrianovendramini@hotmail.com>
2024-02-14 11:29:43 -0300

    atividade 01 de react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import './Home.css'
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Manipulando o mundo</h1>
+      <img
+        className="img"
+        src="https://fotos.amomeupet.org/uploads/fotos/1647927203_62395fa3b8d5d_hd.jpeg"
+      ></img>
+    </>
+  )
+}
+
+export default Home
